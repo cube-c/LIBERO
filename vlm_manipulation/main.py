a16ea0254d7450032f8b8a6676e63274d12a0425
cube-c <cubecube1000@gmail.com>
2025-10-13 01:09:05 +0000

    del

--- vlm_manipulation/main.py
+++ vlm_manipulation/main.py
@@ -1,125 +1,112 @@
-
-from typing import Literal
-
 import os
+from libero.libero import benchmark, get_libero_path
+from libero.libero.envs import OffScreenRenderEnv
 import open3d as o3d
-import tyro
-import torch
-import numpy as np
-import rootutils
+import imageio
 from PIL import Image
+
+import numpy as np
+import torch
+import rootutils
 from loguru import logger as log
 from rich.logging import RichHandler
-from scipy.spatial.transform import Rotation as R
+from collections import OrderedDict
+from robosuite.utils.camera_utils import (
+    get_camera_intrinsic_matrix,
+    get_camera_extrinsic_matrix,
+)
+from vlm_manipulation.curobo_utils import TrajOptimizer
+from curobo.types.robot import JointState
 
 rootutils.setup_root(__file__, pythonpath=True)
 log.configure(handlers=[{"sink": RichHandler(), "format": "{message}"}])
 
-from vlm_manipulation.metasim_utils import ObsSaver, get_pcd_from_rgbd
-from vlm_manipulation.curobo_utils import TrajOptimizer
-from curobo.types.robot import JointState
-from libero.libero import benchmark, get_libero_path, set_libero_default_path
+H, W = 1024, 1024
 
-def get_environment(scenario, sim="isaaclab", robot_offset=1.15):
-    """Initialize the simulation environment."""
-    log.info(f"Using simulator: {sim}")
-    env_class = get_sim_env_class(SimType(sim))
-    env = env_class(scenario)
 
-    robot = scenario.robots[0]
-    init_states, _, _ = get_traj(task, robot, env.handler)
-    init_state = init_states[0]
+def depth_buffer_to_depth_image(depth, zfar, znear):
+    return znear / (1.0 - depth * (1.0 - znear / zfar))
 
-    log.info(
-        f"robot position: {init_state['robots'][robot.name]['pos']} | rotation: {init_state['robots'][robot.name]['rot']}"
+
+def get_pcd_from_rgbd(depth, rgb_img, cam_intr_mat, cam_extr_mat):
+    """Get the point cloud from the RGBD image."""
+    if type(cam_intr_mat) is not np.ndarray:
+        cam_intr_mat = np.array(cam_intr_mat)
+    if type(cam_extr_mat) is not np.ndarray:
+        cam_extr_mat = np.array(cam_extr_mat)
+
+    depth_o3d = o3d.geometry.Image(np.ascontiguousarray(depth).astype(np.float32))
+    rgb_o3d = o3d.geometry.Image(np.ascontiguousarray(rgb_img).astype(np.uint8))
+    rgbd_o3d = o3d.geometry.RGBDImage.create_from_color_and_depth(
+        rgb_o3d, depth_o3d, depth_scale=1.0, convert_rgb_to_intensity=False
     )
-    # log all objects position
-    for obj_name, obj_pos in init_state["objects"].items():
-        log.info(f"object {obj_name} position: {obj_pos['pos']} | rotation: {obj_pos['rot']}")
 
-    # translate robot and all objects to make robot at the origin
-    for obj_name, obj_pos in init_state["objects"].items():
-        init_state["objects"][obj_name]["pos"] -= init_state["robots"][robot.name]["pos"]
-
-    # translate robot to (robot_offset, 0.0, 0.0) and rotate 180 degrees around z axis
-    init_state["robots"][robot.name]["pos"] = torch.tensor([robot_offset, 0.0, 0.0])
-    init_state["robots"][robot.name]["rot"] = torch.tensor([0.0, 0.0, 0.0, 1.0])
-    # translate/rotate all objects relative to robot
-    q_z_180 = torch.tensor([0.0, 1.0, 0.0, 0.0])
-    q_x_180 = torch.tensor([1.0, 0.0, 0.0, 0.0])
-
-    for obj_name, obj_pos in init_state["objects"].items():
-        init_state["objects"][obj_name]["pos"][0] = robot_offset - init_state["objects"][obj_name]["pos"][0] + 0.05
-        init_state["objects"][obj_name]["pos"][1] = -init_state["objects"][obj_name]["pos"][1]
-        if obj_name == "ketchup" or obj_name == "salad_dressing":
-            init_state["objects"][obj_name]["rot"] = torch.tensor(
-                (R.from_quat(q_z_180) * R.from_quat(init_state["objects"][obj_name]["rot"])).as_quat()
-            )
-        if obj_name == "bbq_sauce":
-            init_state["objects"][obj_name]["rot"] = torch.tensor(
-                (R.from_quat(q_x_180) * R.from_quat(init_state["objects"][obj_name]["rot"])).as_quat()
-            )
-
-    log.info(
-        f"[After translation] robot position: {init_state['robots'][robot.name]['pos']} | rotation: {init_state['robots'][robot.name]['rot']}"
+    cam_intr = o3d.camera.PinholeCameraIntrinsic(
+        width=depth.shape[1],
+        height=depth.shape[0],
+        fx=cam_intr_mat[0, 0],
+        fy=cam_intr_mat[1, 1],
+        cx=cam_intr_mat[0, 2],
+        cy=cam_intr_mat[1, 2],
     )
-    for obj_name, obj_pos in init_state["objects"].items():
-        log.info(f"[After translation] object {obj_name} position: {obj_pos['pos']} | rotation: {obj_pos['rot']}")
+    cam_extr = np.array(cam_extr_mat)
 
-    robot.default_position = torch.tensor([robot_offset, 0.0, 0.0])
-    robot.default_orientation = torch.tensor([0.0, 0.0, 0.0, 1.0])
+    pcd = o3d.geometry.PointCloud.create_from_rgbd_image(
+        rgbd_o3d,
+        cam_intr,
+        cam_extr,
+    )
 
-    init_state["robots"][robot.name]["dof_pos"]["panda_finger_joint1"] = 0.04
-    init_state["robots"][robot.name]["dof_pos"]["panda_finger_joint2"] = 0.04
-
-    env.reset(states=[init_state])
-    return env
+    return pcd
 
 
-def get_point_cloud_from_camera(img, depth, camera, output_suffix=""):
+def get_point_cloud_from_camera(img, depth, camera_name):
     """Get the point cloud from the observation."""
-    log.info(f"img shape: {img.shape}, depth shape: {depth.shape}")
-    max_depth = np.max(depth[0].cpu().numpy())
-    scene_depth = depth / max_depth * 255.0  # normalize depth to [0, 1]
-    scene_img = Image.fromarray(img[0].cpu().numpy())
-    scene_depth = Image.fromarray((scene_depth[0].squeeze(-1).cpu().numpy() / max_depth * 255.0).astype("uint8"))
-    scene_img.save(f"vlm_manipulation/output/img{output_suffix}.png")
-    scene_depth.save(f"vlm_manipulation/output/depth{output_suffix}.png")
+    max_depth = np.max(depth)
+    min_depth = np.min(depth)
+    scene_depth = (
+        (depth - min_depth) / (max_depth - min_depth) * 255.0
+    )  # normalize depth to [0, 1]
+    scene_img = Image.fromarray(img)
+    scene_depth = Image.fromarray(scene_depth[:, :, 0].astype(np.uint8))
+    scene_img.save(f"outputs/img_{camera_name}.png")
+    scene_depth.save(f"outputs/depth_{camera_name}.png")
 
-    extr, intr = get_cam_params(
-        cam_pos=torch.tensor([camera.pos]),
-        cam_look_at=torch.tensor([camera.look_at]),
-        width=camera.width,
-        height=camera.height,
-        focal_length=camera.focal_length,
-        horizontal_aperture=camera.horizontal_aperture,
-    )
-    pcd = get_pcd_from_rgbd(depth.cpu()[0], img.cpu()[0], intr[0], extr[0])
+    extr = get_camera_extrinsic_matrix(env.sim, camera_name)
+    extr = np.linalg.inv(extr)
+    intr = get_camera_intrinsic_matrix(env.sim, camera_name, H, W)
+
+    pcd = get_pcd_from_rgbd(depth, img, intr, extr)
     pcd.estimate_normals()
-    log.info(f"pcd shape: {np.array(pcd.points).shape}")
     return pcd, intr, extr
 
 
-def get_point_cloud_from_obs(obs):
-    # check shapes
-    #######################################################################
-    # merge pcds
-    # before merging, we need to align the two point clouds
-    # Align the second viewpoint to the first viewpoint
-    # 1. Get the transformation matrix from the second viewpoint to the first viewpoint
-    # 2. Apply the transformation matrix to the second viewpoint
-    # 3. Merge the two point clouds
-    # 4. Save the merged point cloud
-    depth = obs.cameras["camera0"].depth
+def get_point_cloud_from_obs(obs, zfar, znear, camera_names=["agentview", "sideview"]):
+    depth1 = depth_buffer_to_depth_image(obs[camera_names[0] + "_depth"], zfar, znear)[
+        ::-1
+    ]
+    depth2 = depth_buffer_to_depth_image(obs[camera_names[1] + "_depth"], zfar, znear)[
+        ::-1
+    ]
+    img1 = obs[camera_names[0] + "_image"][::-1]
+    img2 = obs[camera_names[1] + "_image"][::-1]
     pcd1, intr, extr = get_point_cloud_from_camera(
-        obs.cameras["camera0"].rgb, obs.cameras["camera0"].depth, scenario.cameras[0], output_suffix="1"
+        img1,
+        depth1,
+        camera_names[0],
     )
     pcd2, _, _ = get_point_cloud_from_camera(
-        obs.cameras["camera1"].rgb, obs.cameras["camera1"].depth, scenario.cameras[1], output_suffix="2"
+        img2,
+        depth2,
+        camera_names[1],
     )
 
     reg = o3d.pipelines.registration.registration_icp(
-        pcd2, pcd1, 0.0025, np.eye(4), o3d.pipelines.registration.TransformationEstimationPointToPlane()
+        pcd2,
+        pcd1,
+        0.0025,
+        np.eye(4),
+        o3d.pipelines.registration.TransformationEstimationPointToPlane(),
     )
 
     # Convert to numpy arrays
@@ -141,9 +128,24 @@ def get_point_cloud_from_obs(obs):
     pcd_merged = o3d.geometry.PointCloud()
     pcd_merged.points = o3d.utility.Vector3dVector(all_points)
     pcd_merged.colors = o3d.utility.Vector3dVector(all_colors)
-    o3d.io.write_point_cloud("get_started/output/motion_planning/3_object_grasping_vlm/pcd_merged.ply", pcd_merged)
 
-    return pcd_merged, depth[0], intr[0], extr[0]
+    return pcd_merged, depth1, intr, extr
+
+
+def get_joint_state_from_obs(obs):
+    return torch.tensor(
+        np.concatenate([obs["robot0_joint_pos"], obs["robot0_gripper_qpos"]])
+    )
+
+
+def step_to_target_pos(env, obs, target_pos):
+    diff = (target_pos - get_joint_state_from_obs(obs))[:-1]
+
+    # invert for gripper qpos
+    diff[-1] *= -1
+
+    return env.step(diff.tolist())
+
 
 
 class MotionController:
@@ -152,74 +154,114 @@ class MotionController:
     It gets the prompt and return the trajectory.
     It is dependent on the RoboVerse simulator setup.
     """
+
     def __init__(
         self,
-        sim: str,
-        env: IdentityEnvWrapper,
-        obs_saver: ObsSaver,
+        env: OffScreenRenderEnv,
+        obs: OrderedDict,
         traj_optimizer: TrajOptimizer,
     ):
-        self.sim = sim
         self.env = env
-        self.obs_saver = obs_saver
-        self.robot = env.handler.robots[0]
+        self.obs = obs
         self.traj_optimizer = traj_optimizer
+        self.images = []
 
     def dummy_action(self, step: int):
-        return torch.tensor(list(self.robot.default_joint_positions.values())).repeat(step, 1)
-
-    def wrap_action(self, actions):
-        if self.sim == "isaaclab":
-            return [{"dof_pos_target": dict(zip(self.robot.actuators.keys(), action.tolist()))} for action in actions]
-        elif self.sim == "mujoco":
-            return [{self.robot.name: {"dof_pos_target": dict(zip(
-                self.robot.actuators.keys(), action.tolist()
-            ))}} for action in actions]
-        else:
-            raise ValueError(f"Invalid simulator: {self.sim}")
+        return torch.zeros((step, 9))
 
     def get_joint_state(self):
         """Get the current joint position."""
-        joint_reindex = self.env.handler.get_joint_reindex(self.robot.name)
-        joint_pos = self.env.handler.get_states().robots[self.robot.name].joint_pos.cuda()
+        joint_pos = get_joint_state_from_obs(self.obs)
+        joint_pos[8] = -joint_pos[8]
         js = JointState.from_position(
-            position=joint_pos[:, torch.argsort(torch.tensor(joint_reindex))],
-            joint_names=list(self.robot.actuators.keys())
+            position=joint_pos.unsqueeze(0).to(torch.float32).to("cuda:0"),
+            joint_names=[
+                "panda_joint1", 
+                "panda_joint2", 
+                "panda_joint3", 
+                "panda_joint4",
+                "panda_joint5",
+                "panda_joint6",
+                "panda_joint7",
+                "panda_finger_joint1",
+                "panda_finger_joint2",
+            ],
         )
         log.info(f"Current robot joint state: {js}")
         return js
 
     def act(self, actions, save_obs: bool = True):
-        """Act the robot and save the observation"""
-        actions = self.wrap_action(actions)
         for action in actions:
             # log.info(f"Action: {action}")
-            obs, _, _, _, _ = self.env.step([action])
+            obs, _, _, _ = step_to_target_pos(self.env, self.obs, action.detach().cpu())
+            self.obs = obs
             if save_obs:
-                obs = self.env.handler.get_states()
-                self.obs_saver.add(obs)
+                self.images.append(obs["agentview_image"][::-1])
         if save_obs:
-            self.obs_saver.save()
+            self.make_video()
+
         return obs
+
+    def get_point_cloud_from_obs(self):
+        # point cloud from agentview
+        sim = self.env.env.sim
+        extent = float(sim.model.stat.extent)
+        zfar = sim.model.vis.map.zfar * extent
+        znear = sim.model.vis.map.znear * extent
+        pcd, depth, intr, extr = get_point_cloud_from_obs(self.obs, zfar, znear)
+
+        return pcd, depth, intr, extr
+
+    def pcd_to_robot_center(self, pcd):
+        points = np.asarray(pcd.points)
+
+        robot = self.env.env.robots[0]
+        sim = self.env.env.sim
+        bid = sim.model.body_name2id(robot.robot_model.root_body)
+        robot_position = sim.data.body_xpos[bid].copy() 
+        robot_rotation_matrix = sim.data.body_xmat[bid].reshape(3, 3).copy()
+
+        points = points - robot_position
+        points = points @ robot_rotation_matrix.T
+        pcd.points = o3d.utility.Vector3dVector(points)
+        return pcd, robot_position, robot_rotation_matrix
 
     def simulate_from_prompt(self, prompt: str):
         """Simulate the robot from prompt."""
-        actions = self.dummy_action(120)
-        obs = self.act(actions, save_obs=False)
+        actions = self.dummy_action(10)
+        obs = self.act(actions, save_obs=True)
 
-        img = Image.fromarray(obs.cameras["camera0"].rgb[0].cpu().numpy())
-        pcd, depth, cam_intr_mat, cam_extr_mat = get_point_cloud_from_obs(obs)
+        img = Image.fromarray(obs["agentview_image"][::-1])
+        pcd, depth, cam_intr_mat, cam_extr_mat = self.get_point_cloud_from_obs()
+        pcd, robot_position, robot_rotation_matrix = self.pcd_to_robot_center(pcd)
+        o3d.io.write_point_cloud("outputs/merged.ply", pcd)
+
+        # transform camera extrinsic matrix with respect to robot center and rotation matrix
+        T_robot = np.eye(4, dtype=np.float64)
+        T_robot[:3, :3] = robot_rotation_matrix
+        T_robot[:3,  3] = robot_position
+        cam_extr_mat = cam_extr_mat @ T_robot
 
         js = self.get_joint_state()
-        actions = self.traj_optimizer.plan_trajectory(js, img, depth, pcd, prompt, cam_intr_mat, cam_extr_mat)
+        actions = self.traj_optimizer.plan_trajectory(
+            js, img, depth, pcd, prompt, cam_intr_mat, cam_extr_mat
+        )
         obs = self.act(actions, save_obs=True)
         return obs
 
+    def make_video(self):
+        # make video
+        video_writer = imageio.get_writer("outputs/test.mp4", fps=30)
+        for image in self.images:
+            video_writer.append_data(image)
+        video_writer.close()
 
 
 if __name__ == "__main__":
-    prompt = "pick up the bbq sauce and place it in the basket"
-
+    benchmark_root_path = get_libero_path("benchmark_root")
+    init_states_default_path = get_libero_path("init_states")
+    datasets_default_path = get_libero_path("datasets")
+    bddl_files_default_path = get_libero_path("bddl_files")
     benchmark_dict = benchmark.get_benchmark_dict()
     print(benchmark_dict)
 
@@ -227,31 +269,49 @@ if __name__ == "__main__":
     benchmark_instance = benchmark_dict["libero_10"]()
     num_tasks = benchmark_instance.get_num_tasks()
 
-    # 
+    # Load torch init files
+    init_states = benchmark_instance.get_task_init_states(0)
+
+    # task_id is the (task_id + 1)th task in the benchmark
     task_id = 0
-    task = get_task(task_id)
+    task = benchmark_instance.get_task(task_id)
 
-    # default
-    scenario = ScenarioCfg(
-        task=task_name,
-        cameras=[
-            PinholeCameraCfg(name="camera0", width=1024, height=1024, pos=(0.0, 0.0, 0.8), look_at=(0.9, 0.0, 0.0)),
-            PinholeCameraCfg(name="camera1", width=1024, height=1024, pos=(0.9, 1.0, 0.6), look_at=(0.9, 0.0, 0.0)),
-        ],
-        robots=[args.robot],
-        try_add_table=False,
-        sim=args.sim,
-        headless=args.headless,
-        num_envs=args.num_envs,
-    )
-    robot = scenario.robots[0]
-    robot_offset = 1.15
+    env_args = {
+        "bddl_file_name": os.path.join(
+            bddl_files_default_path, task.problem_folder, task.bddl_file
+        ),
+        "camera_names": ["agentview", "sideview"],
+        "camera_depths": True,
+        "camera_heights": H,
+        "camera_widths": W,
+        "controller": "JOINT_POSITION",
+    }
 
-    env = get_environment(scenario, sim=args.sim)
+    env = OffScreenRenderEnv(**env_args)
 
-    ## Main loop
-    os.makedirs("vlm_manipulation/output", exist_ok=True)
-    obs_saver = ObsSaver(video_path=f"vlm_manipulation/output/{task_name}_{args.sim}.mp4")
-    traj_optimizer = TrajOptimizer([robot_offset, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0])
-    motion_controller = MotionController(args.sim, env, obs_saver, traj_optimizer)
-    motion_controller.simulate_from_prompt(prompt)
+    # Controller output limits
+    env.env.robot_configs[0]["controller_config"]["output_min"] = -1.0
+    env.env.robot_configs[0]["controller_config"]["output_max"] = 1.0
+
+    init_states = benchmark_instance.get_task_init_states(task_id)
+
+    # Fix random seeds for reproducibility
+    env.seed(0)
+
+    env.reset()
+
+    for eval_index in range(len(init_states)):
+        images = []
+        obs = env.set_init_state(init_states[eval_index])
+
+        sim = env.env.sim
+        robot = env.env.robots[0]
+
+        traj_optimizer = TrajOptimizer([0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0])
+        mc = MotionController(env, obs, traj_optimizer)
+
+        mc.simulate_from_prompt("pick up the bbq sauce and place it in the basket")
+
+        break
+
+    env.close()
