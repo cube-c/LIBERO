fix quaternion

vlm_manipulation/curobo_utils.py
@@ -458,23 +458,33 @@ class TrajOptimizer:
         positions = grasps.translations.copy()
         rotations = grasps.rotation_matrices.copy()
         quats = R.from_matrix(rotations).as_quat()
-
-        # franka transform
-        franka_L = np.diag([1, -1, 1])
-        franka_R = np.array([[0, 0, 1], [0, 1, 0], [1, 0, 0]])
-        rotations = franka_L @ rotations.transpose(0, 2, 1) @ franka_R
-        # rotations = rotations.transpose(0, 2, 1)
+        log.info(f"Quats: {quats}")
+        rotation_transform_for_franka = torch.tensor(
+            [[
+                [0.0, 0.0, 1.0],
+                [0.0, -1.0, 0.0],
+                [1.0, 0.0, 0.0],
+            ]],
+        )
+        rotations = torch.tensor(rotations, dtype=torch.float32).transpose(2, 1) @ rotation_transform_for_franka
 
         quats = R.from_matrix(rotations).as_quat()
 
-        # convert ee pose from tcp pose
         log.info(f"Positions: {positions}")
         log.info(f"Quats: {quats}")
+
+        # franka transform
+        # franka_L = np.diag([1, -1, 1])
+        # franka_R = np.array([[0, 0, 1], [0, 1, 0], [1, 0, 0]])
+        # rotations = franka_L @ rotations.transpose(0, 2, 1) @ franka_R
+        # rotations = rotations.transpose(0, 2, 1)
+
+        # convert ee pose from tcp pose
         positions, quats = self._ee_pose_from_tcp_pose(
             tcp_pos=torch.tensor(positions, dtype=torch.float32),
             tcp_quat=torch.tensor(quats, dtype=torch.float32),
         )
-        grasp_offset = -torch.cat(
+        grasp_offset = torch.cat(
             [
                 torch.zeros(len(grasps.depths), 2),
                 torch.tensor(grasps.depths, dtype=torch.float32).unsqueeze(1),
@@ -727,7 +737,20 @@ class TrajOptimizer:
         # Pick up
         ee_pos_pickup[:, :, 2] += 0.2
         self.motion_gen.toggle_link_collision(
-            ["panda_hand", "panda_leftfinger", "panda_rightfinger"], False
+            [
+                "panda_link0",
+                "panda_link1",
+                "panda_link2",
+                "panda_link3",
+                "panda_link4",
+                "panda_link5",
+                "panda_link6",
+                "panda_link7",
+                "panda_hand",
+                "panda_leftfinger",
+                "panda_rightfinger",
+            ],
+            False,
         )
         joint_pos.append(
             self.plan_pose_single(
@@ -735,7 +758,20 @@ class TrajOptimizer:
             )
         )
         self.motion_gen.toggle_link_collision(
-            ["panda_hand", "panda_leftfinger", "panda_rightfinger"], True
+            [
+                "panda_link0",
+                "panda_link1",
+                "panda_link2",
+                "panda_link3",
+                "panda_link4",
+                "panda_link5",
+                "panda_link6",
+                "panda_link7",
+                "panda_hand",
+                "panda_leftfinger",
+                "panda_rightfinger",
+            ],
+            False,
         )
         cu_js = self.get_joint_state(joint_pos[-1][-1:, :])
 
@@ -761,16 +797,13 @@ class TrajOptimizer:
             joint_pos = joint_pos[:-1]
 
             rotations = R.from_quat(ee_quat_putdown.squeeze().cpu().numpy()).as_matrix()
-            franka_L = np.diag([1, -1, 1])
-            franka_R = np.array([[0, 0, 1], [0, 1, 0], [1, 0, 0]])
-            rotations = franka_L @ rotations.T @ franka_R
 
             R_180 = np.array([[-1, 0, 0], [0, -1, 0], [0, 0, 1]])
             rotations = R_180 @ rotations @ R_180.T
-            rotations = franka_L @ rotations.T @ franka_R
             ee_quat_putdown = (
                 torch.tensor(R.from_matrix(rotations).as_quat())
                 .to(ee_quat_putdown.device)
+                .to(torch.float32)
                 .unsqueeze(0)
                 .unsqueeze(0)
             )

vlm_manipulation/main.py
@@ -268,19 +268,28 @@ def modify_sideview_camera(env):
     camera_id = sim.model.camera_name2id("sideview")
 
     # Update camera position
-    sim.model.cam_pos[camera_id] = np.array(
-        [-0.05651774593317116, -1.2761224129427358, 0.4879572214102434]
-    )
+    # sim.model.cam_pos[camera_id] = np.array(
+    # [-0.05651774593317116, -1.2761224129427358, 0.4879572214102434]
+    # )
+    # sim.model.cam_quat[camera_id] = np.array(
+    # [
+    # 0.009905065491771751,
+    # -0.006877963156909582,
+    # -0.5912228352893879,
+    # 0.806418094001364,
+    # ]
+    # )
+
+    # manually tuned camera position and rotation
+    sim.model.cam_pos[camera_id] = np.array([0.141838 - 0.6, -0.988357, 0.52037])
     sim.model.cam_quat[camera_id] = np.array(
         [
-            0.009905065491771751,
-            -0.006877963156909582,
-            -0.5912228352893879,
-            0.806418094001364,
+            0.819536,
+            0.507731,
+            -0.139905,
+            -0.225823,
         ]
     )
-
-    # Forward the simulation to apply changes
     sim.forward()
 
 
@@ -290,7 +299,7 @@ if __name__ == "__main__":
     traj_optimizer = TrajOptimizer()
 
     for task_id in range(benchmark_instance.get_num_tasks()):
-        if task_id != 0:
+        if task_id == 0:
             continue
         task = benchmark_instance.get_task(task_id)
         init_states = benchmark_instance.get_task_init_states(task_id)
@@ -317,8 +326,6 @@ if __name__ == "__main__":
         total = 0
 
         for eval_index in range(min(len(init_states), 10)):
-            if eval_index != 0:
-                continue
             #     Fix random seeds for reproducibility
             env.seed(0)
             env.reset()